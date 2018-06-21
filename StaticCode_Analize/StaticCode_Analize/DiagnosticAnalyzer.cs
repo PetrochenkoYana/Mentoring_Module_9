7b9640b0f84d975453b1bb0304e5e936b7722a83
Yana Petrachenka <Yana_Petrachenka@epam.com>
2018-06-21 20:42:52 +0300

    Roslyn

---
+++ StaticCode_Analize/StaticCode_Analize/DiagnosticAnalyzer.cs
@@ -0,0 +1,85 @@
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Threading;
+using System.Web.Mvc;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace StaticCode_Analize
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class StaticCode_AnalizeAnalyzer : DiagnosticAnalyzer
+    {
+        public const string DiagnosticId = "StaticCode_Analize";
+
+        // You can change these strings in the Resources.resx file. If you do not want your analyzer to be localize-able, you can use regular strings for Title and MessageFormat.
+        // See https://github.com/dotnet/roslyn/blob/master/docs/analyzers/Localizing%20Analyzers.md for more on localization
+        private static readonly LocalizableString Title = new LocalizableResourceString(nameof(Resources.AnalyzerTitle), Resources.ResourceManager, typeof(Resources));
+        private static readonly LocalizableString MessageFormat = new LocalizableResourceString(nameof(Resources.AnalyzerMessageFormat), Resources.ResourceManager, typeof(Resources));
+        private static readonly LocalizableString Description = new LocalizableResourceString(nameof(Resources.AnalyzerDescription), Resources.ResourceManager, typeof(Resources));
+        private const string Category = "Naming";
+
+        private static DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, Category, DiagnosticSeverity.Warning, isEnabledByDefault: true, description: Description);
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get { return ImmutableArray.Create(Rule); } }
+
+        public override void Initialize(AnalysisContext context)
+        {
+            // TODO: Consider registering other actions that act on syntax instead of or in addition to symbols
+            // See https://github.com/dotnet/roslyn/blob/master/docs/analyzers/Analyzer%20Actions%20Semantics.md for more information
+            context.RegisterSymbolAction(AnalyzeSymbol, SymbolKind.NamedType);
+            context.RegisterSymbolAction(FindWrongControllerName,SymbolKind.NamedType);
+        }
+
+        private static void FindWrongControllerName(SymbolAnalysisContext context)
+        {
+            var namedTypeSymbol = (INamedTypeSymbol)context.Symbol;
+            var controller = context.Compilation.GetTypeByMetadataName("System.Web.Mvc.ControllerBase,System.Web.Mvc");
+            //var controller = context.Compilation.GetTypeByMetadataName("System.Collections.ArrayList");
+            
+
+            var baseTypes = GetBaseClasses(namedTypeSymbol, context.Compilation.ObjectType);
+
+            // Find just those named type symbols with names containing "Controller".
+            if (baseTypes.Contains(controller) && namedTypeSymbol.Name.EndsWith("Controller"))
+            {
+                // For all such symbols, produce a diagnostic.
+                var diagnostic = Diagnostic.Create(Rule, namedTypeSymbol.Locations[0], namedTypeSymbol.Name);
+
+                context.ReportDiagnostic(diagnostic);
+            }
+        }
+
+        public static ImmutableArray<INamedTypeSymbol> GetBaseClasses(INamedTypeSymbol type, INamedTypeSymbol objectType)
+        {
+            if (type == null || type.TypeKind == TypeKind.Error)
+            {
+                return  ImmutableArray<INamedTypeSymbol>.Empty;
+            }
+
+            if (type.BaseType != null && type.BaseType.TypeKind != TypeKind.Error)
+            {
+                return GetBaseClasses(type.BaseType, objectType).Add(type.BaseType);
+            }
+            return ImmutableArray<INamedTypeSymbol>.Empty;
+        }
+        private static void AnalyzeSymbol(SymbolAnalysisContext context)
+        {
+            // TODO: Replace the following code with your own analysis, generating Diagnostic objects for any issues you find
+            var namedTypeSymbol = (INamedTypeSymbol)context.Symbol;
+
+            // Find just those named type symbols with names containing lowercase letters.
+            if (namedTypeSymbol.Name.ToCharArray().Any(char.IsLower))
+            {
+                // For all such symbols, produce a diagnostic.
+                var diagnostic = Diagnostic.Create(Rule, namedTypeSymbol.Locations[0], namedTypeSymbol.Name);
+
+                context.ReportDiagnostic(diagnostic);
+            }
+        }
+    }
+}
